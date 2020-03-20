5b3e19164e140d13f17bc8db67f12ad6c8dd61c5
shimmy77 <christianshim@gmail.com>
2020-03-20 14:09:23 -0500

    almost

---
+++ seeds/seed.chordful_chords.sql
@@ -0,0 +1,4 @@
+insert into chordful_chords (title, artist, key, tuning, lyrics)
+values 
+    ('Back in Black', 'AC/DC', 'C', 'EADGBE', 'Hey hey hey hey hey'),
+    ("Can't Stop", "RHCP", "E", "EADGBE", "The world I walk, the tears I drop")
